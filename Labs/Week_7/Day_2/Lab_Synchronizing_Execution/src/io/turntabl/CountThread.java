2a2fb6bfedac9f05e68940a18fc5bd38d31aa6c3
Kelvin Morrison <33485727+kelvinninja1@users.noreply.github.com>
2021-03-09 15:45:00 +0000

    Did Second Part and Finished this Lab

--- Labs/Week_7/Day_2/Lab_Synchronizing_Execution/src/io/turntabl/CountThread.java
+++ Labs/Week_7/Day_2/Lab_Synchronizing_Execution/src/io/turntabl/CountThread.java
@@ -1,15 +1,21 @@
 package io.turntabl;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountThread implements Runnable {
     protected int count;
-    public CountThread(int count) {
+    protected CountDownLatch countDownLatch;
+
+    public CountThread(int count, CountDownLatch countDownLatch) {
         this.count = count;
+        this.countDownLatch = countDownLatch;
     }
     @Override
     public void run() {
         try {
             Thread.currentThread().sleep( count * 1000);
             System.out.println("Finished thread " + count);
+            countDownLatch.countDown();
         } catch (InterruptedException ie) { return;}
     }
 }
--- Labs/Week_7/Day_2/Lab_Synchronizing_Execution/src/io/turntabl/Main.java
+++ Labs/Week_7/Day_2/Lab_Synchronizing_Execution/src/io/turntabl/Main.java
@@ -1,14 +1,23 @@
 package io.turntabl;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
 
     private static final int threadCount_ = 5;
+    protected static final CountDownLatch countDownLatch = new CountDownLatch(5);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
         for (int tnum = 1 ; tnum <= threadCount_; ++tnum) {
-            Runnable thread = new CountThread(tnum);
+            Runnable thread = new CountThread(tnum, countDownLatch);
             thread.run();
+
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException ie) {
+            return ;
         }
         System.out.println("Finished starting five threads");
 
